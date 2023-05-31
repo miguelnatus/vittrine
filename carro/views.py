737c4a125e89f1a8d548a18b1ee7395b4dff8924
miguelnatus <miguelnatus@gmail.com>
2023-05-31 07:57:27 -0300

    menu e banne

--- carro/views.py
+++ carro/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Carro, CarroImage
+from .models import Carro, CarroImage, Banner
 from django.shortcuts import get_object_or_404
 def index(request):
     carros = Carro.objects.all()
-    return render(request, 'carro/index.html', {'carros':carros})
+    banners = Banner.objects.all()
+    return render(request, 'carro/index.html', {'carros':carros,'banners':banners})
 
 def carro_view(request, id):
     carro = get_object_or_404(Carro, id=id)
